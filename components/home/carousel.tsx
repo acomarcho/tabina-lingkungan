1b2e1a9961f202867e7f3bd277b78d94d3e23591
acomarcho <acoxstpd@gmail.com>
2023-06-21 23:12:00 +0700

    Feat: Add pages constant

--- components/home/carousel.tsx
+++ components/home/carousel.tsx
@@ -1,9 +1,45 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
+
+const pages = [
+  {
+    title: "KONSULTASI DAN REKAYASA TEKNIK",
+    description:
+      "(Instalasi Air Limbah, Pengontrol Emisi Udara Landfill dengan Pengaman, Design Proses Produksi Bersih 3R)",
+    position: "topRight",
+    images: [
+      "/images/carousel-1-1.png",
+      "/images/carousel-1-2.png",
+      "/images/carousel-1-3.png",
+    ],
+  },
+  {
+    title: "AUDIT LINGKUNGAN HIDUP",
+    description:
+      "(Audit Wajib Berkala Risiko Tinggi,  Audit Wajib/GAKKUM (Mandatory), Audit Sukarela dan Internal, Supervisi Preparasi Audit (Preadit), Supervisi Preparasi PROPER)",
+    position: "topLeft",
+    images: [
+      "/images/carousel-2-1.png",
+      "/images/carousel-2-2.png",
+      "/images/carousel-2-3.png",
+    ],
+  },
+  {
+    title: "PREPARASI, PENYUSUNAN & REVISI DOKUMEN LINGKUNGAN",
+    description:
+      "(Supervisi Preparasi Pertek, Penyusunan & Revisi Dokumen UKL-UPL/AMDAL, Penyusunan Dokumen RKL-RPL,  Supervisi Preparasi Izin Pengelolaan Limbah B3 Pengangkutan, Pengumpulan, Pemanfaatan, Pengolahan, Penimbunan Akhir)",
+    position: "bottomLeft",
+    images: [
+      "/images/carousel-3-1.png",
+      "/images/carousel-3-2.png",
+      "/images/carousel-3-3.png",
+    ],
+  },
+];
 
 export default function Carousel() {
   const [currentPage, setCurrentPage] = useState<number>(0);
-  
+
   return <h1>My carousel component.</h1>;
 }
